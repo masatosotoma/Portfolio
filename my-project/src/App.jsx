68bbf0abbbfc2a0346a251d6f59cdade91d3047a
masatosotoma <masatosotoma@gmail.com>
2024-08-04 23:32:06 -0400

    Adding the card section with svg

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -58,10 +58,26 @@ function App() {
             <p>Looking for an opportunity to be a web Developer</p>
           </div>
         </div>
-        <div className="flex-flex-wrap">
-          <div className="md:w-1/3"></div>
-          <div></div>
-          <div></div>
+        <div className="flex -m-4 flex-wrap">
+          <div className="md:w-1/3 p-4">
+            <div className="bg-gray-100 h-full p-8 rounded-lg ">
+              <div className="flex items-center mb-3">
+                <div className="bg-green-500 text-white rounded-full w-8 h-8 flex justify-center items-center mr-3">
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    className="w-5 h-5"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+                  </svg>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </>
